136e2a667d24fb92ebfdcfeec0ca133647f2f751
HC mbp21 <harrchiu@gmail.com>
2022-03-17 22:26:04 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import webbrowser
+import random
+import os
+import time
+import datetime
+
+
+url = 'https://www.wordleunlimited.com/'
+
+# MacOS
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+#driver = webdriver.Chrome()
+count = 0
+
+for i in range(1):
+
+    webbrowser.get(chrome_path).open(url)
+
+
+
+
+	#driver.get(url)
+	#button = driver.find_element_by_id('idofbutton')
+	#button.click()
+
+	
+
+
